c8e8892b70d1df4e47be12c4a3abf94c6bbdd8b4
tmenka <tomislavmenkadziev@gmail.com>
2024-05-27 16:03:13 +0200

    Pages, Navbar, Footer, CSS-global, User-CD,

--- index.js
+++ index.js
@@ -18,19 +18,63 @@ app.use(express.static('assets'))
 
 
 
-app.get('/', async function (req, res) {//when we visit the root URL express will respond with 'Hello World'
-    const query = 'SELECT * FROM users'
-    const users = await pool.query(query)
-    await res.render('index', { users: users.rows});
+app.get('/', async function (req, res) {
+    await res.render('index');
 
-});//the server is listening on port 3000 for connections
+});
 
-app.get('/kontakt', function (req, res) {//when we visit the root URL express will respond with 'Hello World'
-    res.render('kontakt');
-  });//the server is listening on port 3000 for connections
+app.get('/pocetna', function (req, res) {
+    res.render('index');
+});
 
+app.get('/radno-vrijeme-cjenik', function (req, res) {
+    res.render('priceList');
+});
 
+app.get('/tnt-team', function (req, res) {
+  res.render('tntTeam');
+});
 
+app.get('/galerija', function (req, res) {
+  res.render('galery');
+});
+
+app.get('/kontakt', function (req, res) {
+  res.render('contact');
+});
+
+//ADMIN PART
+app.get('/users', async function (req, res) {
+  const usersQuery = 'SELECT * FROM users';
+  const users = await pool.query(usersQuery);
+
+  const rolesQuery = 'SELECT * FROM roles';
+  const roles = await pool.query(rolesQuery);
+
+  await res.render('admin/users', { users: users.rows, roles: roles.rows });
+});
+
+app.post('/users', async function (req, res) {
+ let user= req.body;
+
+ await pool.query(
+  'INSERT into users (firstname, lastname, username, password, role_id) VALUES($1, $2, $3, $4, $5)', 
+  [user.firstname, user.lastname, user.username, user.password, user.role_id]);
+
+ res.redirect("/users");
+});
+
+app.post('/users/delete', async function (req, res) {
+  let user= req.body;
+ 
+  await pool.query(
+    'DELETE from users where users_id = $1', 
+    [user.users_id]);
+ 
+  res.redirect("/users");
+ });
+
+//the server is listening on port 3000 for connections
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!') //node index.js
 });
