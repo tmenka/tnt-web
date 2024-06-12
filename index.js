abe12b812141bb564023952c157d412d314a9840
tmenka <tomislavmenkadziev@gmail.com>
2024-06-12 13:24:09 +0200

    Gallery - functionalities completed Price list - css updated

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ const pool = new Pool({
 
 //require the just installed express app
 var express = require('express');//then we call express
+var fileUpload = require('express-fileupload');//upload file
+var fs = require('fs');
 var bodyParser = require("body-parser");
 var app = express();//takes us to the root(/) URL
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('assets'))
+app.use(fileUpload());
 
 
 
@@ -115,6 +118,65 @@ app.get('/appointment', async function (req, res) {
   
    res.redirect("/appointment");
   });
+
+
+// GALLERY
+
+app.get('/gallery', async function (req, res) {
+  const galleryQuery = 'SELECT * FROM gallery_items';
+  const galleryDB = await pool.query(galleryQuery);
+
+
+  await res.render('gallery', { gallery_items: galleryDB.rows });
+  });
+
+app.get('/admin/gallery', async function (req, res) {
+  const galleryQuery = 'SELECT * FROM gallery_items';
+  const galleryDB = await pool.query(galleryQuery);
+
+
+  await res.render('admin/admin-gallery', { gallery_items: galleryDB.rows });
+  });
+
+
+app.post('/admin/gallery', async function (req, res) {
+  let uploaded_file = req.files.image;
+  // provjeri jel postoji uplaod file -- uploaded_file
+ let upload_file_name = `${Date.now()}-${uploaded_file.name}`
+
+  uploaded_file.mv('assets/images/galerija/' + upload_file_name, async function(err) {
+    if(err) console.log(err);
+
+    await pool.query(
+      'INSERT into gallery_items (path, title) VALUES($1, $2)', 
+      ['/images/galerija/' + upload_file_name, req.body.title]);
+
+    await res.redirect('/admin/gallery');
+  })
+  
+  });
+
+
+app.post('/admin/gallery/delete', async function (req, res) {
+  let image= req.body;
+   
+  let imageDb = await pool.query(
+    'SELECT * from gallery_items where gallery_items_id = $1', 
+    [image.image_id]);
+
+    fs.unlink('assets' + imageDb.rows[0].path, async function(err) {
+      if(err) console.log(err);
+
+      await pool.query(
+        'DELETE from gallery_items where gallery_items_id = $1', 
+        [image.image_id]);
+        
+        res.redirect("/admin/gallery");
+    })
+
+   
+  });
+
 
 //the server is listening on port 3000 for connections
 app.listen(3000, function () {
