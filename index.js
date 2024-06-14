9cbceb3a47b1a12ab6b270a887607b00e65d6a86
tmenka <tomislavmenkadziev@gmail.com>
2024-06-14 15:29:35 +0200

    Reservation - Grid Reservation - User input Reservation - Admin CRUD

--- index.js
+++ index.js
@@ -38,15 +38,14 @@ app.get('/tnt-team', function (req, res) {
   res.render('tntTeam');
 });
 
-app.get('/galerija', function (req, res) {
-  res.render('galery');
-});
-
 app.get('/kontakt', function (req, res) {
   res.render('contact');
 });
 
 //ADMIN PART
+
+
+// USERS
 app.get('/users', async function (req, res) {
   const usersQuery = 'SELECT * FROM users';
   const users = await pool.query(usersQuery);
@@ -122,7 +121,7 @@ app.get('/appointment', async function (req, res) {
 
 // GALLERY
 
-app.get('/gallery', async function (req, res) {
+app.get('/galerija', async function (req, res) {
   const galleryQuery = 'SELECT * FROM gallery_items';
   const galleryDB = await pool.query(galleryQuery);
 
@@ -176,6 +175,146 @@ app.post('/admin/gallery/delete', async function (req, res) {
 
    
   });
+
+
+
+  // REZERVACIJA
+
+  app.get('/rezervacija', async function (req, res) {
+    const appointmentQuery = `SELECT app.* FROM appointment app 
+                              LEFT JOIN reservation res ON app.appointment_id = res.appointment_id
+                              WHERE res.reservation_id IS NULL`;
+    const appointmentsDb = await pool.query(appointmentQuery);
+  
+  
+     await res.render('reservation', { appointments: appointmentsDb.rows });
+  });
+
+  app.post('/reservation', async function (req, res) {
+    let reservation = req.body;
+   
+    await pool.query(
+     'INSERT into reservation (firstname, lastname, email, phone_number,' + 
+     'date_of_birth, allergies, comment, status, appointment_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)', 
+     [reservation.firstname, reservation.lastname, reservation.email,
+      reservation.phone_number, reservation.date_of_birth, reservation.allergies,
+      reservation.comment, 0, reservation.appointment_id]);
+   
+    res.redirect("/rezervacija");
+   });
+
+  //  ADMIN RESERVATIONS
+
+  app.get('/admin/reservation', async function (req, res) {
+    const reservationQuery = `SELECT res.*, app.start_date_time, app.end_date_time FROM appointment app 
+                              INNER JOIN reservation res ON app.appointment_id = res.appointment_id
+                              WHERE res.status = 1`;
+    const reservationsDb = await pool.query(reservationQuery);
+  
+  
+     await res.render('admin/admin-reservations', { reservations: reservationsDb.rows });
+  });
+
+  app.post('/reservations/delete', async function (req, res) {
+    let reservation = req.body;
+   
+    await pool.query(
+      'DELETE from reservation where reservation_id = $1', 
+      [reservation.reservation_id]);
+   
+    res.redirect("/admin/reservation");
+   });
+
+   app.post('/reservations/change-status', async function (req, res) {
+    let reservation= req.body;
+   
+    await pool.query(
+      'UPDATE reservation SET status = 0 WHERE reservation_id = $1', 
+      [reservation.reservation_id]);
+   
+    res.redirect("/admin/reservation");
+   });
+
+
+     //  ADMIN PRE-RESERVATIONS
+
+  app.get('/admin/pre-reservation', async function (req, res) {
+    const pre_reservationQuery = `SELECT res.*, app.start_date_time, app.end_date_time FROM appointment app 
+                              INNER JOIN reservation res ON app.appointment_id = res.appointment_id
+                              WHERE res.status = 0`;
+    const pre_reservationsDb = await pool.query(pre_reservationQuery);
+  
+  
+     await res.render('admin/admin-pre-reservations', { pre_reservations: pre_reservationsDb.rows });
+  });
+
+  app.post('/pre-reservations/delete', async function (req, res) {
+    let pre_reservation= req.body;
+   
+    await pool.query(
+      'DELETE from reservation where reservation_id = $1', 
+      [pre_reservation.reservation_id]);
+   
+    res.redirect("/admin/pre-reservation");
+   });
+
+
+   app.post('/pre-reservations/change-status', async function (req, res) {
+    let pre_reservation= req.body;
+   
+    await pool.query(
+      'UPDATE reservation SET status = 1 WHERE reservation_id = $1', 
+      [pre_reservation.reservation_id]);
+   
+    res.redirect("/admin/pre-reservation");
+   });
+
+
+  //  EDIT PRE/RESERVATIONS
+
+  app.get('/admin/reservation/:reservation_id', async function (req, res) {
+    let reservation_id = parseInt(req.params.reservation_id)
+    const reservationQuery = `SELECT res.*, app.start_date_time, app.end_date_time FROM appointment app 
+                              INNER JOIN reservation res ON app.appointment_id = res.appointment_id
+                              WHERE res.reservation_id = $1`;
+    const reservationsDb = await pool.query(reservationQuery,[reservation_id]);
+
+    const appointmentQuery = `SELECT * FROM appointment`;
+    const appointmentsDb = await pool.query(appointmentQuery);
+
+  
+  
+     await res.render('admin/edit-reservation', { reservation: reservationsDb.rows[0], appointments: appointmentsDb.rows});
+  });
+
+  app.post('/admin/reservation/:reservation_id', async function (req, res) {
+    let reservation_id = parseInt(req.params.reservation_id);
+    let reservation = req.body;
+    console.log(111111, reservation)
+    console.log(222222, reservation_id)
+    await pool.query(
+      `UPDATE reservation SET 
+          firstname = $1, 
+          lastname =$2, 
+          email = $3, 
+          phone_number = $4,
+          date_of_birth = $5,
+          allergies = $6, 
+          comment =$7, 
+          status = $8, 
+          appointment_id = $9
+        WHERE reservation_id = $10`, 
+      [reservation.firstname, reservation.lastname, reservation.email,
+       reservation.phone_number, reservation.date_of_birth, reservation.allergies,
+       reservation.comment, reservation.status, reservation.appointment_id, reservation_id]);
+
+       if (reservation.status == 0){
+        res.redirect("/admin/pre-reservation")
+       }
+       else{
+        res.redirect("/admin/reservation")
+       }
+    });
 
 
 //the server is listening on port 3000 for connections
