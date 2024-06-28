50a6bdbe0a419dd35da4acebbed4ad045b817e50
tmenka <tomislavmenkadziev@gmail.com>
2024-06-28 13:36:15 +0200

    removed some comments btn styling title styling change credentials, employee edit user, admin roles seperated FIXED:	overlaping appointments, 	time between appointments, 	issue with "CE" same username of users,

--- index.js
+++ index.js
@@ -72,7 +72,6 @@ app.get('/rezervacija', async function (req, res) {
 app.post('/reservation', async function (req, res) {
   let reservation = req.body;
 
-  // TODO dodat check za appointment
    
   await pool.query(
      'INSERT into reservation (firstname, lastname, email, phone_number,' + 
@@ -84,7 +83,7 @@ app.post('/reservation', async function (req, res) {
   res.redirect("/rezervacija");
 });
 
-// LOGIN
+    // LOGIN
 
 app.get('/login', async function (req, res) {
      let errorMessage = req.query.errorMessage ?? null; 
@@ -103,8 +102,9 @@ app.post('/login', async function (req, res) {
 
     if (usersDb && usersDb.rows.length > 0 && bcrypt.compareSync(password, usersDb.rows[0].password)) {
         req.session.loggedin = true;
-        req.session.username = username;
-        res.redirect('admin/users');
+        req.session.user_id =  usersDb.rows[0].users_id;
+        req.session.isAdmin = usersDb.rows[0].role_id == 1;
+        res.redirect('/admin');
     } else {
       res.redirect('/login?errorMessage=Pogrešno korisničko ime i/ili zaporka');
     }    
@@ -124,7 +124,7 @@ app.get('/logout', (req, res) => {
 
 //--------------ADMIN PART---------------
 
-// Middleware to protect /admin routes
+    // Middleware to protect /admin routes
 app.use('/admin', (req, res, next) => {
     if (req.session.loggedin) {
         next();
@@ -133,9 +133,65 @@ app.use('/admin', (req, res, next) => {
     }
 });
 
-// users
+app.get('/admin', async function (req, res) {
+  
+  if(req.session.isAdmin)
+  {
+    res.redirect('/admin/users')
+  } else {
+    res.redirect('/admin/appointment')
+  }
+});
+
+
+
+// PROFILE
+
+app.get('/admin/profile', async function (req, res) {
+  let errorMessage = req.query.errorMessage ?? null;
+
+  const usersQuery = 'SELECT * FROM users WHERE users_id = $1';
+  const users = await pool.query(usersQuery, [req.session.user_id]);
+
+
+  await res.render('admin/profile', { user: users.rows[0] , errorMessage: errorMessage, isAdmin: req.session.isAdmin});
+});
+
+app.post('/admin/profile', async function (req, res) {
+  let user = req.body;
+  const usersDb = await pool.query( 'SELECT * FROM users WHERE username = $1 AND users_id!=$2', [user.username, req.session.user_id]);
+ 
+  if (usersDb && usersDb.rows.length > 0) { 
+     return res.redirect("/admin/profile?errorMessage=Korisnik već postoji");
+  }  
+  if (user.password) {
+    await pool.query(
+    'UPDATE users SET firstname = $1, lastname = $2, username = $3, password = $4 WHERE users_id = $5', 
+    [user.firstname, user.lastname, user.username, bcrypt.hashSync(user.password, 10), req.session.user_id]);
+  
+      res.redirect("/admin/profile");
+   } else {
+    await pool.query(
+      'UPDATE users SET firstname = $1, lastname = $2, username = $3 WHERE users_id = $4', 
+      [user.firstname, user.lastname, user.username, req.session.user_id]);
+    
+       res.redirect("/admin/profile");
+   }
+});
+
+
+
+    // USERS
+
 app.get('/admin/users', async function (req, res) {
-  let errorMessage = req.query.errorMessage ?? null; 
+  let errorMessage = req.query.errorMessage ?? null;
+  let edit_user_id = req.query.edit_user_id ?? null;
+
+  
+  if(!req.session.isAdmin)
+  {
+     return res.redirect('/admin')
+  }
 
   const usersQuery = 'SELECT * FROM users';
   const users = await pool.query(usersQuery);
@@ -143,14 +199,14 @@ app.get('/admin/users', async function (req, res) {
   const rolesQuery = 'SELECT * FROM roles';
   const roles = await pool.query(rolesQuery);
 
-  await res.render('admin/users', { users: users.rows, roles: roles.rows , errorMessage: errorMessage  });
+  await res.render('admin/users', { users: users.rows, roles: roles.rows , errorMessage: errorMessage, isAdmin: req.session.isAdmin, edit_user_id: edit_user_id});
 });
 
 app.post('/admin/users', async function (req, res) {
  let user = req.body;
  const usersDb = await pool.query( 'SELECT * FROM users WHERE username = $1', [user.username]);
  
- if (usersDb && usersDb.rows.length > 0 && bcrypt.compareSync(user.password, usersDb.rows[0].password)) { 
+ if (usersDb && usersDb.rows.length > 0) { 
     res.redirect("/admin/users?errorMessage=Korisnik već postoji");
  } else { 
    await pool.query(
@@ -159,6 +215,29 @@ app.post('/admin/users', async function (req, res) {
 
     res.redirect("/admin/users");
  }
+});
+
+app.post('/admin/users/edit', async function (req, res) {
+  let user= req.body;
+  const usersDb = await pool.query( 'SELECT * FROM users WHERE username = $1 AND users_id!=$2', [user.username, user.user_id]);
+ 
+  if (usersDb && usersDb.rows.length > 0) { 
+     return res.redirect("/admin/users?errorMessage=Korisnik već postoji");
+  }  
+  if (user.password) {
+    await pool.query(
+    'UPDATE users SET firstname = $1, lastname = $2, username = $3, password = $4, role_id = $5 WHERE users_id = $6', 
+    [user.firstname, user.lastname, user.username, bcrypt.hashSync(user.password, 10), user.role_id, user.user_id]);
+  
+      res.redirect("/admin/users");
+   } else {
+    await pool.query(
+      'UPDATE users SET firstname = $1, lastname = $2, username = $3, role_id = $4 WHERE users_id = $5', 
+      [user.firstname, user.lastname, user.username, user.role_id, user.user_id]);
+    
+       res.redirect("/admin/users");
+   
+   }
 });
 
 app.post('/admin/users/delete', async function (req, res) {
@@ -172,7 +251,9 @@ app.post('/admin/users/delete', async function (req, res) {
 });
 
 
-//appointment
+
+    //APPOINTMENT
+
 app.get('/admin/appointment', async function (req, res) {
   let errorMessage = req.query.errorMessage ?? null; 
   let edit_app_id = req.query.edit_appointment_id ?? null;
@@ -180,44 +261,56 @@ app.get('/admin/appointment', async function (req, res) {
   const appointmentsDb = await pool.query(appointmentQuery);
 
   await res.render('admin/appointment', {
-     appointments: appointmentsDb.rows, edit_appointment_id: edit_app_id, errorMessage: errorMessage
-  });
+     appointments: appointmentsDb.rows, edit_appointment_id: edit_app_id, errorMessage: errorMessage, isAdmin: req.session.isAdmin});
 });
 
  app.post('/admin/appointment', async function (req, res) {
   let appointment= req.body;
+
+  let start_date_time = new Date(appointment.start_date_time)
+  let end_date_time = new Date(appointment.end_date_time)
+
+  if(appointment.end_date_time < appointment.start_date_time ){
+    return res.redirect("/admin/appointment?errorMessage=Kraj termina ne moze biti prije pocetka termina");
+  }
  
-  // TODO jel mos imat 2 appointment u isto vijeme ili da jedan appointment ulazi u drugi
-  // const appointmentsDb = await pool.query('SELECT * FROM appointment WHERE (start_date, end_date) OVERLAPS ($1, $2)',
-  //   [appointment.start_date_time, appointment.end_date_time]);
+  const appointmentsDb = await pool.query('SELECT * FROM appointment WHERE (start_date_time, (end_date_time + INTERVAL \'1 hour\')) OVERLAPS ($1, $2)',
+    [appointment.start_date_time, appointment.end_date_time]);
   
-  // if(appointmentsDb && appointmentsDb.rows.length > 0) {
-  //   res.redirect("/admin/appointment?errorMessage=Termin je zauzet");
-  // } else {
+  if(appointmentsDb && appointmentsDb.rows.length > 0) {
+    res.redirect("/admin/appointment?errorMessage=Termin je zauzet");
+  } else {
     await pool.query(
      'INSERT into appointment (start_date_time, end_date_time) VALUES($1, $2)', 
      [appointment.start_date_time, appointment.end_date_time]);
    
     res.redirect("/admin/appointment");
-  // }
+   }
 
 });
 
  app.post('/admin/appointment/edit', async function (req, res) {
-  let appointment= req.body;
-  // TODO jel mos imat 2 appointment u isto vijeme ili da jedan appointment ulazi u drugi
-  // const appointmentsDb = await pool.query('SELECT * FROM appointment WHERE (start_date, end_date) OVERLAPS ($1, $2)',
-  //   [appointment.start_date_time, appointment.end_date_time]);
+  let appointment = req.body;
+
+  if(appointment.end_date_time < appointment.start_date_time ){
+    return res.redirect("/admin/appointment?errorMessage=Kraj termina ne moze biti prije pocetka termina");
+  }
+
+  const appointmentsDb = await pool.query('SELECT * FROM appointment WHERE (start_date_time, (end_date_time + INTERVAL \'1 hour\')) OVERLAPS ($1, $2)',
+    [appointment.start_date_time, appointment.end_date_time]);
   
-  // if(appointmentsDb && appointmentsDb.rows.length > 0) {
-  //   res.redirect("/admin/appointment?errorMessage=Termin je zauzet");
-  // } else {
+
+  if(appointmentsDb && appointmentsDb.rows.length > 0) {
+    return res.redirect("/admin/appointment?errorMessage=Termin je zauzet");
+  } 
+  
+  else {
   await pool.query(
    'UPDATE appointment SET start_date_time = $1, end_date_time = $2 WHERE appointment_id = $3', 
    [appointment.start_date_time, appointment.end_date_time, appointment.appointment_id]);
  
     res.redirect("/admin/appointment");
-  // }
+   }
 });
  
  app.post('/admin/appointment/delete', async function (req, res) {
@@ -231,7 +324,8 @@ app.get('/admin/appointment', async function (req, res) {
 });
 
 
-// gallery
+
+    // GALLERY
 
 app.get('/admin/gallery', async function (req, res) {
   let errorMessage = req.query.errorMessage ?? null; 
@@ -239,13 +333,13 @@ app.get('/admin/gallery', async function (req, res) {
   const galleryDB = await pool.query(galleryQuery);
 
 
-  await res.render('admin/admin-gallery', { gallery_items: galleryDB.rows, errorMessage: errorMessage });
+  await res.render('admin/admin-gallery', { gallery_items: galleryDB.rows, errorMessage: errorMessage, isAdmin: req.session.isAdmin });
 });
 
 
 app.post('/admin/gallery', async function (req, res) {
   let uploaded_file = req.files.image;
-  // provjeri jel postoji uplaod file -- uploaded_file
+  // provjera uploada
   let upload_file_name = `${Date.now()}-${uploaded_file.name}`;
   if (!uploaded_file) {
     res.redirect("/admin/gallery?errorMessage=No file uploaded");
@@ -286,7 +380,8 @@ app.post('/admin/gallery/delete', async function (req, res) {
 });
 
 
-  //  ADMIN RESERVATIONS
+
+    // ADMIN RESERVATIONS
 
 app.get('/admin/reservation', async function (req, res) {
   let errorMessage = req.query.errorMessage ?? null; 
@@ -296,7 +391,7 @@ app.get('/admin/reservation', async function (req, res) {
   const reservationsDb = await pool.query(reservationQuery);
   
   
-    await res.render('admin/admin-reservations', { reservations: reservationsDb.rows, errorMessage: errorMessage });
+    await res.render('admin/admin-reservations', { reservations: reservationsDb.rows, errorMessage: errorMessage, isAdmin: req.session.isAdmin });
 });
 
   app.post('/admin/reservations/delete', async function (req, res) {
@@ -320,7 +415,7 @@ app.get('/admin/reservation', async function (req, res) {
    });
 
 
-     //  ADMIN PRE-RESERVATIONS
+     // ADMIN PRE-RESERVATIONS
 
   app.get('/admin/pre-reservation', async function (req, res) {
     let errorMessage = req.query.errorMessage ?? null; 
@@ -330,7 +425,7 @@ app.get('/admin/reservation', async function (req, res) {
     const pre_reservationsDb = await pool.query(pre_reservationQuery);
   
   
-     await res.render('admin/admin-pre-reservations', { pre_reservations: pre_reservationsDb.rows, errorMessage: errorMessage });
+     await res.render('admin/admin-pre-reservations', { pre_reservations: pre_reservationsDb.rows, errorMessage: errorMessage, isAdmin: req.session.isAdmin });
   });
 
   app.post('/admin/pre-reservations/delete', async function (req, res) {
@@ -355,7 +450,7 @@ app.get('/admin/reservation', async function (req, res) {
    });
 
 
-  //  EDIT PRE/RESERVATIONS
+      //  EDIT PRE/RESERVATIONS
 
   app.get('/admin/reservation/:reservation_id', async function (req, res) {
     let errorMessage = req.query.errorMessage ?? null; 
@@ -371,7 +466,7 @@ app.get('/admin/reservation', async function (req, res) {
   
   
      await res.render('admin/edit-reservation', { 
-        reservation: reservationsDb.rows[0], appointments: appointmentsDb.rows, errorMessage: errorMessage});
+        reservation: reservationsDb.rows[0], appointments: appointmentsDb.rows, errorMessage: errorMessage, isAdmin: req.session.isAdmin});
   });
 
   app.post('/admin/reservation/:reservation_id', async function (req, res) {
@@ -402,10 +497,17 @@ app.get('/admin/reservation', async function (req, res) {
        }
     });
 
+
   // middleware for not found
   app.use('*', (req, res) => {
     res.redirect('/');
   });
+
+
+
+
+
+
 
 //the server is listening on port 3000 for connections
 app.listen(3000, function () {
